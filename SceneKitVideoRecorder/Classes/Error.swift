b00e3006dff4ab5b04b2a6855e7954ab0bb2df4c
Fabricio Oliveira <netwolf.fabricio@gmail.com>
2018-05-17 15:38:00 -0300

    Adapting to add the meta data info on the video to make it similar to imessage`s videos sent by animoji. Metadata: Loop and GREY.

--- SceneKitVideoRecorder/Classes/Error.swift
+++ SceneKitVideoRecorder/Classes/Error.swift
@@ -7,12 +7,12 @@
 import UIKit
 
 extension SceneKitVideoRecorder {
-  public enum ErrorCode: Int {
-    case notReady = 0
-    case zeroFrames = 1
-    case assetExport = 2
-    case recorderBusy = 3
-    case unknown = 4
-  }
+    public enum ErrorCode: Int {
+        case notReady = 0
+        case zeroFrames = 1
+        case assetExport = 2
+        case recorderBusy = 3
+        case unknown = 4
+    }
 }
 
